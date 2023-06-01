cc5aa653b3e2d0e2db4d55cb0768855c6e6cb421
Joe Kabban <133786476+JoeKabban@users.noreply.github.com>
2023-06-01 20:36:35 +0300

    Update Code-Rocco.ino

--- Code-Rocco.ino
+++ Code-Rocco.ino
@@ -1,5 +1,5 @@
 int relay1ControlPin = 7; // Pin connected to relay 1 
-int relay2ControlPin = 8; // Pin connected to relay 2 
+int relay2ControlPin = 8; // Pin connected to relay 2  
 char command;             // Variable to store incoming commands
 
 void setup() {
@@ -16,19 +16,20 @@ void loop() {
     // Control the relays/motors based on the received command
     if (command == 'F') {
       // Action 1: Open both motors
-      digitalWrite(relay1ControlPin, LOW); // Turn relay 1 off
+      // our relay1 is reversed so when it's high it means that the relay is off and when it's low it means that the relay is on
+      digitalWrite(relay1ControlPin, LOW); // Turn relay 1 on
       digitalWrite(relay2ControlPin, HIGH); // Turn relay 2 on
     } else if (command == 'S') {
       // Action 2: Close both motors
-      digitalWrite(relay1ControlPin, HIGH);  // Turn relay 1 on
+      digitalWrite(relay1ControlPin, HIGH);  // Turn relay 1 off
       digitalWrite(relay2ControlPin, LOW);  // Turn relay 2 off
     } else if (command == 'R') {
       // Action 3: Open motor 1 and close motor 2
-      digitalWrite(relay1ControlPin, LOW); // Turn relay 1 off
+      digitalWrite(relay1ControlPin, LOW); // Turn relay 1 on
       digitalWrite(relay2ControlPin, LOW);  // Turn relay 2 off
     } else if (command == 'L') {
       // Action 4: Close motor 1 and open motor 2
-      digitalWrite(relay1ControlPin, HIGH);  // Turn relay 1 on
+      digitalWrite(relay1ControlPin, HIGH);  // Turn relay 1 off
       digitalWrite(relay2ControlPin, HIGH); // Turn relay 2 on
     }
   }
